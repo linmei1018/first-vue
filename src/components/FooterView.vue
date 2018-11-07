5fe7275b97a4bf201086c038c74b638e1d2b0b03
linmei <1098188079@qq.com>
2018-11-07 14:23:51 +0800

    修改底部文字和添加图标

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -2,23 +2,27 @@
   <footer>
     <mt-tabbar>
       <router-link to="/home" tag="div">
-        <mt-tab-item id="首页">
-          首页
+        <mt-tab-item id="收藏">
+          <Icon type="ios-star-outline" size="26" />
+          <p>收藏</p>
         </mt-tab-item>
       </router-link>
       <router-link to="/news" tag="div">
-        <mt-tab-item id="新闻">
-          新闻
+        <mt-tab-item id="最近通话">
+          <Icon type="ios-time-outline" size="26" />
+          <p>最近通话</p>
         </mt-tab-item>
       </router-link>
       <router-link to="/fav" tag="div">
         <mt-tab-item id="通讯录">
-          通讯录
+          <Icon type="ios-contact" size="26" />
+          <p>通讯录</p>
         </mt-tab-item>
       </router-link>
       <router-link to="/my" tag="div">
         <mt-tab-item id="我的">
-          我的
+          <Icon type="ios-keypad-outline" size="26" />
+          <p>拨号键盘</p>
         </mt-tab-item>
       </router-link>
     </mt-tabbar>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 Vue.use(MintUI)
+
+Vue.use(iView)
 
 import axios from 'axios'
 Vue.prototype.$http = axios

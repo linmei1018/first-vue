0ee7e8d4a86ed306b0a0b6eb9822604be01edd26
linmei <1098188079@qq.com>
2018-11-08 10:41:18 +0800

    通讯录添加搜索功能

--- src/components/Home/AddressBookView.vue
+++ src/components/Home/AddressBookView.vue
@@ -1,99 +1,190 @@
 <template>
   <div>
-    <mt-header fixed title="通讯录"></mt-header>
+    <div class="header" :class="isClick?'HeaderHidden':''" >
+      <p style="width:95%">通讯录</p>
+      <Icon type="md-add" size="20" style="line-height:40px;" @click="addSearch" />
+    </div>
+    <!--添加通讯录弹窗-->
+    <mt-popup
+      v-model="popupVisible"
+      class="add-search-popup"
+      position="bottom">
+      13456
+    </mt-popup>
+    <div :class="isClick?'shadowShow':''" @click="shadowCancel" ></div>
+    <div :class="isChange?'shadowCancel':'changeSearch'">
+      <p>123</p>
+    </div>
+    <div class="search_name" :class="isClick?'search':''" >
+      <div class="inputSearch" @click="searchName">
+        <Input class="search-key"
+               v-model.trim="searchKey"
+               @on-change="searchChange()"
+               placeholder="搜索"
+               icon="ios-search"/>
+      </div>
+      <span class="cancelSearch" @click="cancelSearch">取消</span>
+      <!--<div class="search-key">-->
+        <!--<input @on-change="searchChange()" v-model.trim="searchKey" placeholder="搜索"/>取消-->
+      <!--</div>-->
+
+    </div>
     <div class="mail_content">
-      <p>本机号码：13879136593</p>
+      <h3 style="height:40px;line-height:40px;">本机号码：13879136593</h3>
       <mt-index-list>
-        <mt-index-section index="A">
-          <mt-cell title="Aaron"></mt-cell>
-          <mt-cell title="Alden"></mt-cell>
-          <mt-cell title="Austin"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="B">
-          <mt-cell title="Baldwin"></mt-cell>
-          <mt-cell title="Braden"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="C">
-          <mt-cell title="Coding"></mt-cell>
-          <mt-cell title="Coke"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="D">
-          <mt-cell title="Diss"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="E">
-          <mt-cell title="Eally"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="F">
-          <mt-cell title="Faldwin"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="G">
-          <mt-cell title="Gain"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="H">
-          <mt-cell title="Hald"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="I">
-          <mt-cell title="Idwin"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="J">
-          <mt-cell title="Join"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="K">
-          <mt-cell title="Kamily"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="L">
-          <mt-cell title="Leary"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="M">
-          <mt-cell title="Merealy"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="N">
-          <mt-cell title="Baldwin"></mt-cell>
-          <mt-cell title="Braden"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="O">
-          <mt-cell title="Orize"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="P">
-          <mt-cell title="Pappty"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="Q">
-          <mt-cell title="Query"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="R">
-          <mt-cell title="Road"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="S">
-          <mt-cell title="Simal"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="T">
-          <mt-cell title="Tittle"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="U">
-          <mt-cell title="Undkown"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="V">
-          <mt-cell title="Voinery"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="W">
-          <mt-cell title="Wellcome"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="X">
-          <mt-cell title="Xinline"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="Y">
-          <mt-cell title="Yang"></mt-cell>
-        </mt-index-section>
-        <mt-index-section index="Z">
-          <mt-cell title="Zack"></mt-cell>
-          <mt-cell title="Zane"></mt-cell>
+        <mt-index-section v-for="item in result" :index="item.value" :key="item.value">
+          <mt-cell v-for="(items,index) in item.title" :key="index" :title="items"></mt-cell>
         </mt-index-section>
       </mt-index-list>
     </div>
+
   </div>
 </template>
+<script>
+  export default {
+    data(){
+      return{
+        value:'',
+        isClick: false, //是否点击搜索框
+        isChange:false, //
+        searchKey:'',
+        result:[
+          { value:'A',title:['Aaron','Alden','Austin']},
+          { value:'B',title:['Baby']},
+          { value:'C',title:['Colly']},
+          { value:'D',title:['Diar']},
+          { value:'E',title:['Ender']},
+          { value:'F',title:['Function']},
+          { value:'G',title:['Green']},
+          { value:'H',title:['Heigth']},
+          { value:'I',title:['Index']},
+          { value:'J',title:['Join']},
+          { value:'K',title:['Koll']},
+          { value:'L',title:['Light']},
+          { value:'M',title:['Menney']},
+          { value:'N',title:['Nothing']},
+          { value:'O',title:['Orige']},
+          { value:'P',title:['Patty']},
+          { value:'Q',title:['Query']},
+          { value:'R',title:['Road']},
+          { value:'S',title:['Son']},
+          { value:'T',title:['Title']},
+          { value:'U',title:['Unkown']},
+          { value:'V',title:['Vobbly']},
+          { value:'W',title:['Wellcome']},
+          { value:'X',title:['Xinxin']},
+          { value:'Y',title:['Yang']},
+          { value:'Z',title:['Zone']}
+        ],
+        searchDataCopy:'',  //副本
+        searchData:'', //搜索数据
+        popupVisible:false, //添加通讯录弹窗
+      }
+    },
+    methods:{
+      //点击搜索框事件
+      searchName(){
+        this.isClick = true;
+      },
+      //关闭遮罩层
+      shadowCancel(){
+        this.isClick = false;
+      },
+      //搜索框Change事件
+      searchChange(){
+        this.isChange = true;
+      },
+      //取消搜索
+      cancelSearch(){
+        this.isClick = false;
+        this.isChange = false;
+        this.searchKey = '';
+      },
+      //添加通讯录
+      addSearch(){
+        this.popupVisible = true;
+      }
+    }
+  }
+</script>
 <style scoped>
+  .header{
+    top: 0;
+    right: 0;
+    left: 0;
+    position: fixed;
+    z-index: 1;
+    display: flex;
+    font-size: 14px;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 10px;
+    background-color: #26a2ff;
+    color:#fff;
+    text-align:center;
+  }
+  .add-search-popup{
+    width:100%;
+    height:100%;
+  }
+  .HeaderHidden{
+    display:none;
+  }
+  .changeSearch{
+    display:none;
+  }
+  .shadowShow{
+    height:100%;
+    width:100%;
+    position:fixed;
+    background:#000;
+    opacity:0.5;
+    z-index: 1;
+  }
+  .shadowCancel{
+    height:100%;
+    width:100%;
+    position:fixed;
+    top:60px;
+    background:#fff;
+    z-index: 1;
+    display:block;
+  }
+  .mint-search{
+    height:100%;
+    margin-top:-20px;
+  }
+  .search_name{
+    margin-top:-20px;
+    padding: 5px 10px;
+    background:#ccc;
+    width:100%;
+    position:absolute;
+  }
+  .inputSearch{
+    float:left;
+    width:90%;
+  }
+  .cancelSearch{
+    display:inline-block;
+    color: #5394ec;
+    font-weight:bold;
+    font-size:14px;
+    height:50px;
+    line-height:50px;
+  }
+  .search-key{
+    margin:10px 0;
+    width:80%;
+  }
+  .search{
+    padding: 5px 10px;
+    background:#ccc;
+    margin-top:-60px;
+    z-index: 10;
+  }
   .mint-indexsection{
     text-align:left;
   }
+
 </style>
